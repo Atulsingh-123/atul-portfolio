8bca9d6ef4df3510697f4065749ea84412d9c491
unknown <atul.singh@voyatekgroup.com>
2024-07-29 00:53:10 +0530

    header changes

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar: React.FC = () => {
                     <div className="rounded-full bg-white h-8 w-8 sm:h-10 sm:w-10 flex items-center justify-center ml-1">
                         <img src={Logo} alt="Portfolio Logo" className="h-6 sm:h-10" />
                     </div>
-                    <div className="text-white text-xl sm:text-2xl font-extrabold tracking-wider ml-2">My Portfolio</div>
+                    <div className="text-white text-xl sm:text-2xl font-extrabold tracking-wider ml-2">Portfolio</div>
                 </div>
                 <div className="space-x-4 sm:space-x-8">
                     <a href="#about" className="text-white text-sm sm:text-lg hover:text-gray-200 transition-colors duration-300">About</a>
