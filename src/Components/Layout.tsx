3b667a97cc5f739ea5cd9206494e4fe819f1676f
unknown <atul.singh@voyatekgroup.com>
2024-07-29 00:12:22 +0530

    some error resolved during deployment

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -1,7 +1,6 @@
 // src/components/Layout.tsx
 import React, { ReactNode } from 'react';
 import Navbar from './Navbar';
-import Footer from './Footer';
 
 interface LayoutProps {
     children: ReactNode;
